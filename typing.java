03b88d856e1d804500da13516ade1ccc2f24da75
EdwardNing46290 <74420451+EdwardNing46290@users.noreply.github.com>
2022-05-23 19:17:31 -0400

    Update typing.java
    Just do stuff I have no idea what to do

--- typing.java
+++ typing.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
 
+// do we need a class here?
+public static void main (String[] args{ {
+  // initialize variables and stuff
+  
+  // Buffer Reader
+  
+  // add a method which can read our inputs and compare it to the text file
+  
+  // add methods which display: percentage correct + incorrect, time taken, average wpm, average correct wpm, # of characters written?
+  
+  // allow them to do backspaces, and confirm the word written whenever they type a space
+  
+  // time limit typing tests: 0:30, 1:00, 3:00, 5:00...?
+  
+  // give ppl an animal image based on their accuracy, wpm...?
+  // turtle, horse, cheetah, sea anemone?
+  
+  // add inspirational message about their typing: you will improve, youre doing well, really good, etc.
+  
+  // try and figure out how to make the game look good with formatting or images
+}
