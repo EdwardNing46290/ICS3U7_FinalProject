f18fb5af1caec56df06cbb47d148d1266ccb5033
EdwardNing46290 <74420451+EdwardNing46290@users.noreply.github.com>
2022-06-15 01:51:43 -0400

    Update SignUp.java

--- src/SignUp.java
+++ src/SignUp.java
@@ -1,1 +1,41 @@
+import javax.swing.*;
+import java.awt.Font;
+import java.awt.event.ActionListener; // should take input?
+import java.io.*;
 
+public class SignUp implements ActionListener {
+
+	public JTextField usernameText;
+	public JPasswordField passwordText;
+  
+  // Times New Roman
+	Font roman = new Font("TimesRoman", Font.PLAIN, 20);
+	Font buttonRoman = new Font("TimesRoman", Font.PLAIN, 15);
+  
+	/**
+    SignUp() {
+    // Username and Password
+		JFrame signUpFrame = new JFrame();
+		usernameText = new JUsernameField("");
+		passwordText = new JPasswordField("");
+		signUp = new JButton("SignUp");
+		signUpFrame.setSize(200,200);
+    
+    // setBounds
+		JLabel username = new JLabel("Username");
+		username.setBounds(__, __, __, __);
+		username.setFont(TimesRoman);
+		loginFrame.add(username);
+		JLabel password = new JLabel("Password");
+		password.setBounds(__, __, __, __);
+    password.setFont(TimesRoman);
+		loginFrame.add(password);
+    
+    // ... create a username + password and store it for the login
+    potential multiplayer mode?
+*/
+
+		login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+}
