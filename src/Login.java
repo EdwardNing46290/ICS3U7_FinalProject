2f218a2d9b3c7917f480d23e9394c4e2cad1dab3
EdwardNing46290 <74420451+EdwardNing46290@users.noreply.github.com>
2022-06-15 01:49:04 -0400

    Create Login.java
    Login for Java

---
+++ src/Login.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+import java.awt.Font;
+import java.awt.event.ActionListener; // should take input?
+import java.io.*;
+
+public class Login implements ActionListener {
+
+	public JTextField usernameText;
+	public JPasswordField passwordText;
+  
+  // Times New Roman
+	Font roman = new Font("TimesRoman", Font.PLAIN, 20);
+	Font buttonRoman = new Font("TimesRoman", Font.PLAIN, 15);
+  
+	/**
+    Login() {
+    // Username and Password
+		JFrame loginFrame = new JFrame();
+		usernameText = new JUsernameField("");
+		passwordText = new JPasswordField("");
+		login = new JButton("Login");
+
+		loginFrame.setSize(200,200);
+    
+    // setBounds
+		JLabel username = new JLabel("Username");
+		username.setBounds(__, __, __, __);
+		username.setFont(TimesRoman);
+		loginFrame.add(username);
+
+		JLabel password = new JLabel("Password");
+		password.setBounds(__, __, __, __);
+    password.setFont(TimesRoman);
+		loginFrame.add(password);
+    
+    // ... check if the username exists and password is correct
+    boolean exists = tempFile.exists(); ?
+*/
+
+		login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+}
