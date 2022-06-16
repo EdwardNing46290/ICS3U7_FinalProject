9f6bdc4fc3aff008d500667e0da0d4e505515d82
EdwardNing46290 <74420451+EdwardNing46290@users.noreply.github.com>
2022-06-15 21:43:42 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,9 @@ import java.io.IOException;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		
+		MainMenu menuPage = new MainMenu();
 		// Compile word bank for text generation
-		WordBankCompiler.compileBank();
+		// WordBankCompiler.compileBank();
 		
 		// Build GUI
 		JFrame frame = new JFrame("WPM test");
