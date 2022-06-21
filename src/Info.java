76105a7404a7da9b81df0ba5c7aac29550ce7a58
EdwardNing46290 <74420451+EdwardNing46290@users.noreply.github.com>
2022-06-21 09:12:29 -0400

    Update Info.java

--- src/Info.java
+++ src/Info.java
@@ -1,2 +1,21 @@
 // just print out info similiar to MonkeyType
-JTextField infoText = new JTextField;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Color;
+
+public class Info implements ActionListener {
+  JLabel infoText = new JLabel;
+  public Info () throws MalformedURLException{
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
