dcee7546d1b74a63196954196aee868b57280e01
EdwardNing46290 <74420451+EdwardNing46290@users.noreply.github.com>
2022-06-22 10:47:03 -0400

    Update Info.java

--- src/Info.java
+++ src/Info.java
@@ -20,9 +20,10 @@ public class Info implements ActionListener {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     		JLabel infoText = new JLabel();
 		JFrame frame = new JFrame();
+		JLabel info = new JLabel("Created by Vincent Liu and Edward Ning");
 		JLabel signUpInfo = new JLabel("To sign up, enter a username and password; no email required");
 		JLabel loginInfo = new JLabel("To log in, enter the username and password you used to sign up previously");
-		JLabel settingsInfo = new JLabel("I have no idea rn")
+		JLabel settingsInfo = new JLabel("I have no idea rn");
 		JLabel scoreInfo = new JLabel("Stats shown: WPM (words per minute), percentage of correct words, and total words");
 		JLabel contactInfo = new JLabel("If you have any problems with our test, you can contact us here ___ play rickroll");
-		JLabel wordsInfo = new JLabel("We use the 100 most common english words for easy mode, 200 for medium, and 500 more difficult words for hard")
+		JLabel wordsInfo = new JLabel("We use the 100 most common english words for easy mode, 200 for medium, and 500 more difficult words for hard");
