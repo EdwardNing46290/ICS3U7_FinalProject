ef5e9825c6694e0923451a6eab0ecaf16dd9e563
EdwardNing46290 <74420451+EdwardNing46290@users.noreply.github.com>
2022-06-23 04:01:09 -0400

    Update Login.java

--- src/Login.java
+++ src/Login.java
@@ -1,148 +1,88 @@
-/* import javax.swing.*;
+import java.awt.Color;
 import java.awt.Font;
-import java.awt.event.ActionListener; // should take input?
-import java.io.*;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
+import java.util.HashMap;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 
 public class Login implements ActionListener {
-
+	
+	static String file = "login.txt";
 	public JTextField usernameText;
-	public JPasswordField passwordText;
-  
-  // Times New Roman
+	public JTextField passwordText;
+	public HashMap<String,String> loginInfo;
+	
+	// Times New Roman
 	Font roman = new Font("TimesRoman", Font.PLAIN, 20);
-	Font buttonRoman = new Font("TimesRoman", Font.PLAIN, 15);
-  
-	/**
-    Login() {
-    // Username and Password
+
+	Login() {
+		// Username and Password
 		JFrame loginFrame = new JFrame();
-		usernameText = new JUsernameField("");
-		passwordText = new JPasswordField("");
-		JTextField username = new JTextField("Enter your Username: ");
-		JTextField password = new JTextField("Enter your Password: ");
-		login = new JButton("Login");
+		loginFrame.setSize(550,300);
+		loginFrame.setBackground(Color.GREEN);
 
+		JTextField usernameText = new JTextField("");
+		JLabel usernameLabel = new JLabel();  
+		usernameLabel.setText("Username");
+
+		passwordText = new JTextField("");
+		JLabel passwordLabel = new JLabel();  
+		passwordLabel.setText("Password");
+
+		JTextField textField = new JTextField(15);
+		JTextField usernameInput = new JTextField("Enter your Username: ");
+		JTextField passwordInput = new JTextField("Enter your Password: ");
+        String usernameValue = usernameInput.getText();
+        String passwordValue = passwordInput.getText(); 
+		JButton login = new JButton("Login");
 		loginFrame.setSize(200,200);
-    
-    // setBounds
-		JLabel username = new JLabel("Username");
-		username.setBounds(__, __, __, __);
-		username.setFont(TimesRoman);
-		loginFrame.add(username);
 
-		JLabel password = new JLabel("Password");
-		password.setBounds(__, __, __, __);
-    password.setFont(TimesRoman);
-		loginFrame.add(password);
-    
-    // ... check if the username exists and password is correct
-    boolean exists = tempFile.exists(); ?
+		// setBounds
+		JLabel usernameDisplay = new JLabel("Username");
+		// usernameDisplay.setBounds(__, __, __, __);
+		// usernameDisplay.setFont(TimesRoman);
+		loginFrame.add(usernameInput);
+		JLabel passwordDisplay = new JLabel("Password");
+		// passwordDisplay.setBounds(__, __, __, __);
+		// passwordDisplay.setFont(TimesRoman);
+		loginFrame.add(passwordInput);
 
+		JButton confirm = new JButton("SUBMIT");
+		HashMap loginInformation = new HashMap<>();
+		// ... check if the username exists and password is correct
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(login.txt));
+			String username = "";
+			String password = "";
+			String seperateUserPass [];
+			while (reader.hasNextLine) != null) {
+				seperateUserPass = currentLine.split(" ");
+				username = seperateUserPass[0];
+				password = seperateUserPass[1];
+				loginInformation.put(username, password);
 
-		login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			}
+			// some login code from Javatpoint
+
+		} catch (Exception IOX) {
+			JOptionPane.showMessageDialog(loginFrame, "Error, something is wrong with the login.");
+		}
+		// error code from tutorialspoint.com
 
 	}
 }
-//import required classes and packages  
-import javax.swing.*;  
-import java.awt.*;  
-import java.awt.event.*;  
-import java.lang.Exception;  
-  
-//create CreateLoginForm class to create login form  
-//class extends JFrame to create a window where our component add  
-//class implements ActionListener to perform an action on button click  
-class CreateLoginForm extends JFrame implements ActionListener  
-{  
-    //initialize button, panel, label, and text field  
-    JButton b1;  
-    JPanel newPanel;  
-    JLabel userLabel, passLabel;  
-    final JTextField  textField1, textField2;  
-      
-    //calling constructor  
-    CreateLoginForm()  
-    {     
-          
-        //create label for username   
-        userLabel = new JLabel();  
-        userLabel.setText("Username");      //set label value for textField1  
-          
-        //create text field to get username from the user  
-        textField1 = new JTextField(15);    //set length of the text  
-  
-        //create label for password  
-        passLabel = new JLabel();  
-        passLabel.setText("Password");      //set label value for textField2  
-          
-        //create text field to get password from the user  
-        textField2 = new JPasswordField(15);    //set length for the password  
-          
-        //create submit button  
-        b1 = new JButton("SUBMIT"); //set label to button  
-          
-        //create panel to put form elements  
-        newPanel = new JPanel(new GridLayout(3, 1));  
-        newPanel.add(userLabel);    //set username label to panel  
-        newPanel.add(textField1);   //set text field to panel  
-        newPanel.add(passLabel);    //set password label to panel  
-        newPanel.add(textField2);   //set text field to panel  
-        newPanel.add(b1);           //set button to panel  
-          
-        //set border to panel   
-        add(newPanel, BorderLayout.CENTER);  
-          
-        //perform action on button click   
-        b1.addActionListener(this);     //add action listener to button  
-        setTitle("LOGIN FORM");         //set title to the login form  
-    }  
-      
-    //define abstract method actionPerformed() which will be called on button click   
-    public void actionPerformed(ActionEvent ae)     //pass action listener as a parameter  
-    {  
-        String userValue = textField1.getText();        //get user entered username from the textField1  
-        String passValue = textField2.getText();        //get user entered pasword from the textField2  
-          
-        //check whether the credentials are authentic or not  
-        if (userValue.equals("test1@gmail.com") && passValue.equals("test")) {  //if authentic, navigate user to a new page  
-              
-            //create instance of the NewPage  
-            NewPage page = new NewPage();  
-              
-            //make page visible to the user  
-            page.setVisible(true);  
-              
-            //create a welcome label and set it to the new page  
-            JLabel wel_label = new JLabel("Welcome: "+userValue);  
-            page.getContentPane().add(wel_label);  
-        }  
-        else{  
-            //show error message  
-            System.out.println("Please enter valid username and password");
-	    // code borrowed from javatpoint
-        }  
-    }  
-}  
-//create the main class  
-class LoginFormDemo  
-{  
-    //main() method start  
-    public static void main(String arg[])  
-    {  
-        try  
-        {  
-            //create instance of the CreateLoginForm  
-            CreateLoginForm form = new CreateLoginForm();  
-            form.setSize(300,100);  //set size of the frame  
-            form.setVisible(true);  //make form visible to the user  
-        }  
-        catch(Exception e)  
-        {     
-            //handle exception   
-            JOptionPane.showMessageDialog(null, e.getMessage()); 
-	    https://www.javatpoint.com/login-form-java 
-	    https://www.geeksforgeeks.org/java-swing-simple-user-registration-form/
-        }  
-    }  
-} 
-*/
