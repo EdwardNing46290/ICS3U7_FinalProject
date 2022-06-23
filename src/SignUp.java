77ee549e35cf207ad6de7cc4586c52270204944a
EdwardNing46290 <74420451+EdwardNing46290@users.noreply.github.com>
2022-06-23 04:43:56 -0400

    Update SignUp.java

--- src/SignUp.java
+++ src/SignUp.java
@@ -1,43 +1,117 @@
-/*
+import java.util.HashMap;
+
 import javax.swing.*;
+
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.Font;
-import java.awt.event.ActionListener; // should take input?
+import java.awt.PopupMenu;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
 import java.io.*;
 
-public class SignUp implements ActionListener {
 
+public class SignUp implements ActionListener{
+
+	static String file = "login.txt";
 	public JTextField usernameText;
-	public JPasswordField passwordText;
-  
-  // Times New Roman
-	Font roman = new Font("TimesRoman", Font.PLAIN, 20);
-	Font buttonRoman = new Font("TimesRoman", Font.PLAIN, 15);
-  
-	/**
-    SignUp() {
-    // Username and Password
-		JFrame signUpFrame = new JFrame();
-		usernameText = new JUsernameField("");
-		passwordText = new JPasswordField("");
-		signUp = new JButton("SignUp");
-		signUpFrame.setSize(200,200);
-    
-    // setBounds
-		JLabel username = new JLabel("Username");
-		username.setBounds(__, __, __, __);
-		username.setFont(TimesRoman);
-		loginFrame.add(username);
-		JLabel password = new JLabel("Password");
-		password.setBounds(__, __, __, __);
-    password.setFont(TimesRoman);
-		loginFrame.add(password);
-    
-    // ... create a username + password and store it for the login
-    potential multiplayer mode?
+	public JTextField passwordText;
+	public HashMap <String, String> signUpInfo;
 
-		login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		// pretty much the same as login code rn
+	void Signup() {
+		JFrame signupFrame = new JFrame();
+		Component signUpFrame;
+		signUpFrame.setSize(900, 800);
+		HashMap signUpInfo = new HashMap<> ();
+		usernameText = new JTextField("");
+		passwordText = new JTextField("");
+		JButton signUp = new JButton("Sign up");
+
+		//store all the info from the text file into the hashmap
+
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(file));
+			String username = "";
+			String password = "";
+			String seperateUserPass[];
+			while (reader.hasNextLine) {
+				//put information into hashmap
+				String currentLine;
+				seperateUserPass = currentLine.split(" ");
+				username = seperateUserPass[0];
+				password = seperateUserPass[1];
+				signUpInfo.put(username, password);
+
+			}
+
+
+		} catch (Exception IOX) {
+			JOptionPane.showMessageDialog(signupFrame, "Error, sign in could not register");
+		}
+
+		JLabel usernameDisplay = new JLabel("Username");
+		// usernameDisplay.setBounds(__, __, __, __);
+		// usernameDisplay.setFont(TimesRoman);
+		Component loginFrame;
+		PopupMenu usernameInput;
+		loginFrame.add(usernameInput);
+
+		JLabel passwordDisplay = new JLabel("Password");
+		// passwordDisplay.setBounds(__, __, __, __);
+		// passwordDisplay.setFont(TimesRoman);
+		PopupMenu passwordInput;
+		loginFrame.add(passwordInput);
+
+		usernameText.setBounds(260,60,180,45);
+		signupFrame.add(usernameText);
+
+		passwordText.setBounds(260,130,180,45);
+		signupFrame.add(passwordText);
+
+		signUp.setBounds(400,400,400,400);
+		signUp.setBackground(Color.white);
+		signUp.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e)
+			{
+				if (e.getSource() == signUp) {
+					String username = usernameText.getText();
+					String password = passwordText.getText();
+
+
+					signUpInfo.put(username, password);
+
+					//put the new account information into the text file
+					try {
+						BufferedWriter text = new BufferedWriter(new FileWriter(file,true));
+						text.write(username + " " + password);
+
+						text.close();
+						signupFrame.dispose();
+
+					} catch (IOException iox) {
+						JOptionPane.showMessageDialog(signupFrame, "Error");
+
+					}
+				}
+			}
+
+		}
+
+
+				);
+
+		signupFrame.add(signUp);
+		signupFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 	}
-}
 
-	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+*/
