edc5d93872b8295e17ab4f1a9ac448f8eb76fc09
EdwardNing46290 <74420451+EdwardNing46290@users.noreply.github.com>
2022-06-24 21:45:14 -0400

    Update SignUp.java

--- src/SignUp.java
+++ src/SignUp.java
@@ -1,8 +1,11 @@
 import java.io.*;
+import java.net.MalformedURLException;
+
 import javax.swing.*;
 import java.awt.Color;
 import java.util.HashMap;
 import java.awt.Component;
+import java.awt.Font;
 import java.awt.PopupMenu;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,18 +16,25 @@ public class SignUp {
 	static JTextField usernameText;
 	static JTextField passwordText;
 	static HashMap <String, String> signUpInfo;
-	
+
+
 	// Times New Roman
-	Font roman = new Font("TimesRoman", Font.PLAIN, 20);
-	
-	public static void SignUp() {
+
+	public static void SignUp() throws IOException {
 		JFrame signUpFrame = new JFrame();
 		signUpFrame.setSize(900, 800);
 
+		signUpFrame.setLayout(null);
+		signUpFrame.setVisible(true);
+		signUpFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		signUpFrame.setTitle("Sign Up Menu");
+		signUpFrame.setResizable(false);
+
 		HashMap signUpInfo = new HashMap<> ();
-		usernameText = new JTextField("");
-		passwordText = new JTextField("");
-		JButton signUp = new JButton("Sign up");
+		usernameText = new JTextField("Type Here");
+		passwordText = new JTextField("Type Here");
+		JButton signUp = new JButton("Sign Up");
 
 		//store all the info from the text file into the hashmap
 
@@ -50,57 +60,79 @@ public class SignUp {
 		}
 
 		JLabel usernameDisplay = new JLabel("Username");
-		usernameDisplay.setBounds(100, 100, 200, 50);
-		usernameDisplay.setFont(roman);
-		Component loginFrame;
-		PopupMenu usernameInput;
-		loginFrame.add(usernameInput);
+		usernameDisplay.setBounds(100, 300, 200, 50);
+		usernameDisplay.setFont(new Font("Times New Roman", Font.BOLD, 30));
+		// Component loginFrame;
+		// PopupMenu usernameInput;
+		signUpFrame.add(usernameDisplay);
 
 		JLabel passwordDisplay = new JLabel("Password");
-		passwordDisplay.setBounds(100, 120, 200, 50);
-		passwordDisplay.setFont(roman);
-		PopupMenu passwordInput;
-		loginFrame.add(passwordInput);
+		passwordDisplay.setBounds(100, 400, 200, 50);
+		passwordDisplay.setFont(new Font("Times New Roman", Font.BOLD, 30));
+		// PopupMenu passwordInput;
+		signUpFrame.add(passwordDisplay);
 
-		usernameText.setBounds(300, 100, 180, 45);
+		signUp.setBounds(300, 500, 200, 75);
+		signUp.setFocusable(false);
+		signUp.setFont(new Font("Times New Roman", Font.PLAIN, 30));
+		signUp.setForeground(Color.WHITE);
+		signUp.setBackground(Color.BLACK);
+
+		usernameText.setBounds(250, 300, 400, 50);
 		signUpFrame.add(usernameText);
 
-		passwordText.setBounds(300, 120, 200, 50);
+		passwordText.setBounds(250, 400, 400, 50);
 		signUpFrame.add(passwordText);
 
-		signUp.setBounds(400, 400, 400, 400);
-		signUp.setBackground(Color.white);
 		signUp.addActionListener(new ActionListener() {
 
-			 public void actionPerformed(ActionEvent e)
-				 {
-					 if (e.getSource() == signUp) {
-						 String username = usernameText.getText();
-						 String password = passwordText.getText();
+			public void actionPerformed(ActionEvent e)
+			{
+				if (e.getSource() == signUp) {
+					String username = usernameText.getText();
+					String password = passwordText.getText();
 
 
-						 signUpInfo.put(username, password);
+					signUpInfo.put(username, password);
 
-						 //put the new account information into the text file
-						 try {
-							 BufferedWriter text = new BufferedWriter(new FileWriter(file,true));
-							 text.write(username + " " + password);
+					//put the new account information into the text file
+					try {
+						BufferedWriter text = new BufferedWriter(new FileWriter(file,true));
+						text.write(username + " " + password);
 
-							 text.close();
-							 signUpFrame.dispose();
+						signUp.addActionListener(new ActionListener() {
+							public void actionPerformed(ActionEvent e) {
+								try {
+									signUpFrame.dispose();
+									new MainMenu();
+								} catch (MalformedURLException e1) {
+									// TODO Auto-generated catch block
+									e1.printStackTrace();
+								}
+							}
+						});
+						text.close();
 
-						 } catch (IOException iox) {
-							 JOptionPane.showMessageDialog(signUpFrame, "Error");
+					} catch (IOException iox) {
+						JOptionPane.showMessageDialog(signUpFrame, "Error");
 
-						 }
-					 }
-				 }
+					}
+				}
+			}
 
-			 }
-		);
+		}
+				);
 
 		signUpFrame.add(signUp);
+		signUpFrame.add(usernameText);
+		signUpFrame.add(passwordText);
 		signUpFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+
+	public static void addActionListener(ActionListener signIn) {
+		// TODO Auto-generated method stub
 
 	}
 }
