33f9f4f65999d9f6ff162797b618e7d34d3cacb1
EdwardNing46290 <74420451+EdwardNing46290@users.noreply.github.com>
2022-06-24 22:49:51 -0400

    Update Login.java

--- src/Login.java
+++ src/Login.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.HeadlessException;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -23,46 +24,61 @@ import javax.swing.JTextField;
 public class Login {
 
 	static String file = "login.txt";
-	public JTextField usernameText;
-	public JTextField passwordText;
-	public HashMap<String,String> loginInfo;
+	public static JTextField usernameText;
+	public static JTextField passwordText;
+	public static HashMap<String,String> loginInfo;
 
 	// Times New Roman
 	Font roman = new Font("TimesRoman", Font.PLAIN, 20);
 
-	Login() {
+	public static void Login() throws IOException {
 		// Username and Password
 		JFrame loginFrame = new JFrame();
-		loginFrame.setSize(900,800);
+		loginFrame.setSize(900, 800);
 		loginFrame.setBackground(Color.GREEN);
 
-		JTextField usernameText = new JTextField("");
+		loginFrame.setLayout(null);
+		loginFrame.setVisible(true);
+		loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JLabel usernameDisplay = new JLabel("Username");
+		usernameDisplay.setBounds(100, 300, 200, 50);
+		usernameDisplay.setFont(new Font("Times New Roman", Font.BOLD, 30));
+		// Component loginFrame;
+		// PopupMenu usernameInput;
+		loginFrame.add(usernameDisplay);
+
+		JLabel passwordDisplay = new JLabel("Password");
+		passwordDisplay.setBounds(100, 400, 200, 50);
+		passwordDisplay.setFont(new Font("Times New Roman", Font.BOLD, 30));
+		// PopupMenu passwordInput;
+		loginFrame.add(passwordDisplay);
+
+		JButton login = new JButton("Login");
+
+		login.setBounds(300, 500, 200, 75);
+		login.setFocusable(false);
+		login.setFont(new Font("Times New Roman", Font.PLAIN, 30));
+		login.setForeground(Color.WHITE);
+		login.setBackground(Color.BLACK);
+
+		JTextField usernameInput = new JTextField("");
 		JLabel usernameLabel = new JLabel();
 		usernameLabel.setText("Username");
 
-		passwordText = new JTextField("");
+		JTextField passwordInput = new JTextField("");
 		JLabel passwordLabel = new JLabel();
 		passwordLabel.setText("Password");
 
-		JTextField textField = new JTextField(15);
-		JTextField usernameInput = new JTextField("Enter your Username: ");
-		JTextField passwordInput = new JTextField("Enter your Password: ");
-		String usernameValue = usernameInput.getText();
-		String passwordValue = passwordInput.getText();
-		JButton login = new JButton("Login");
-		loginFrame.setSize(200,200);
-
-		// setBounds
-		JLabel usernameDisplay = new JLabel("Username");
-		usernameDisplay.setBounds(100, 100, 200, 50);
-		usernameDisplay.setFont(roman);
+		usernameInput.setBounds(250, 300, 400, 50);
 		loginFrame.add(usernameInput);
-		JLabel passwordDisplay = new JLabel("Password");
-		passwordDisplay.setBounds(100, 120, 200, 50);
-		passwordDisplay.setFont(roman);
+
+		passwordInput.setBounds(250, 400, 400, 50);
 		loginFrame.add(passwordInput);
 
-		JButton confirm = new JButton("Submit Password");
+		String usernameValue = usernameInput.getText();
+		String passwordValue = passwordInput.getText();
+
 		HashMap loginInformation = new HashMap<>();
 		// ... check if the username exists and password is correct
 		try {
@@ -80,7 +96,53 @@ public class Login {
 				loginInformation.put(username, password);
 
 			}
+			login.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					if (e.getSource() == login) {
+						String username = usernameText.getText();
+						String password = passwordText.getText();
+
+						//check if the username exists 
+						if(loginInfo.containsKey(username)) {
+							try {
+								if((loginInfo.get(username).equals(password))) {
+									JOptionPane.showMessageDialog(loginFrame, "Login successful!");
+									loginFrame.dispose();
+								}
+								else {
+									JOptionPane.showMessageDialog(loginFrame, "Login unsuccessful.");
+								}
+							} catch (HeadlessException e1) {
+								// TODO Auto-generated catch block
+								e1.printStackTrace();
+							}
+						}
+					}
+				}
+			}
 			// some login code from Javatpoint
+
+			JButton home = new JButton("Main Menu");
+
+			home.setBounds(660, 20, 200, 75);
+			home.setFocusable(false);
+			home.setFont(new Font("Times New Roman", Font.PLAIN, 30));
+
+			home.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					try {
+						loginFrame.dispose();
+						new MainMenu();
+					} catch (MalformedURLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+			});
+
+			loginFrame.add(home);
+			loginFrame.add(login);
+			loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		} catch (Exception IOX) {
 			JOptionPane.showMessageDialog(loginFrame, "Error, something is wrong with the login.");
